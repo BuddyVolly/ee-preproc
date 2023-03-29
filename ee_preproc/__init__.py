fbd97ed0df7d0f4e3082129f92892e42d39a42a0
BuddyVolly <andreasvollrath@gmx.de>
2023-03-29 13:00:08 +0000

    chore: setting up automaization of commits

--- ee_preproc/__init__.py
+++ ee_preproc/__init__.py
@@ -4,11 +4,4 @@ __version__ = "0.0.0"
 __author__ = "Andreas Vollrath"
 __email__ = "andreas.vollrath@fao.org"
 
-
-def hello_world() -> str:
-    """Hello world demo method.
-
-    Returns:
-        the hello world string
-    """
-    return "hello world !"
+from .landsat.landsat_collection import landsat_collection as landsat_collection
